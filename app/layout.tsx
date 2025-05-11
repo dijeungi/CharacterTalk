ae22223bce841893cb2fe3ef4022f67e9f1d0602
dijeungi <dijeungi@naver.com>
2025-05-11 15:57:56 +0900

    :lipstick: font 추가 및 전역 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist } from "next/font/google";
 
 // css
 import "./styles/common/globals.css";
+import "./config/fonts";
 import "./reset.css";
 
 // 함수git ad
@@ -14,9 +15,13 @@ import { useRenderHeader } from "@/components/header/HeaderManager";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
-// Geist Sans 폰트 설정
+// 폰트 설정
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const roboto = Geist({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
@@ -30,7 +35,7 @@ export default function RootLayout({
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <html lang="ko" className={geistSans.variable}>
+    <html lang="ko" className={`${geistSans.variable} ${roboto.variable}`}>
       <body>
         <QueryClientProvider client={queryClient}>
           {renderHeader()}
