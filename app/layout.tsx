f279d6091d5fe6f86e994c268af4363251b9a2be
dijeungi <dijeungi@naver.com>
2025-05-22 07:46:23 +0000

    🚚 layout.tsx > provider로 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,25 @@
 // app/layout.tsx
 
-"use client";
+import './reset.css';
+import '@/_config/fonts';
+import { Geist } from 'next/font/google';
+import { ReactNode } from 'react';
+import ClientProviders from '@/_components/provider/ClientProviders';
 
-// font
-import { Geist } from "next/font/google";
-
-// css
-import "./reset.css";
-import "@/_config/fonts";
-
-// 함수
-import { useRenderHeader } from "@/_components/header/HeaderManager";
-
-// 라이브러리
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-
-// 폰트 설정
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 const roboto = Geist({
-  variable: "--font-roboto",
-  subsets: ["latin"],
+  variable: '--font-roboto',
+  subsets: ['latin'],
 });
 
-// 모든 페이지에 적용될 레이아웃 컴포넌트
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const renderHeader = useRenderHeader();
-  const [queryClient] = useState(() => new QueryClient());
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" className={`${geistSans.variable} ${roboto.variable}`}>
       <body>
-        <QueryClientProvider client={queryClient}>
-          {renderHeader()}
-          <main>{children}</main>
-        </QueryClientProvider>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
