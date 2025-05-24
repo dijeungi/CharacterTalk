72c94d9e38807a60b7a451d50f7c35db73e44741
dijeungi <dijeungi@naver.com>
2025-05-24 06:45:16 +0000

    🔥 사용 안하는 파일 삭제

--- app/_config/ApiConfig.tsx
+++
@@ -1,4 +0,0 @@
-// config/ApiConfig.ts
-
-export const API_SERVER_HOST = process.env.NEXT_PUBLIC_API_URL;
-export const AI_SERVER_HOST = process.env.NEXT_PUBLIC_AI_SERVER_URL;
--- app/_utils/generateSessionKey.tsx
+++
@@ -1,8 +0,0 @@
-// lib/toss/encryption.ts
-import crypto from "crypto";
-
-/** 32바이트 세션키 생성 후 base64 인코딩 */
-export function generateSessionKey(): string {
-  const key = crypto.randomBytes(32);
-  return key.toString("base64");
-}
