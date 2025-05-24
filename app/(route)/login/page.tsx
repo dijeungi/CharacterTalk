f938b32cbf5d1f378b37279621502fa9858a1627
dijeungi <dijeungi@naver.com>
2025-05-24 06:46:19 +0000

    🎨 줄바꿈 수정

--- app/(route)/login/page.tsx
+++ app/(route)/login/page.tsx
@@ -1,8 +1,8 @@
 // app/auth/login/page.tsx
 
-"use client";
+'use client';
 
-import styles from "@/_styles/auth/Login.module.css";
+import styles from '@/_styles/auth/Login.module.css';
 
 export default function LoginPage() {
   const handleKakaoLogin = () => {
@@ -14,16 +14,11 @@ export default function LoginPage() {
     <div className={styles.container}>
       <div className={styles.title}>
         <h1 className={styles.top_title}>다시, 안녕</h1>
-        <h4 className={styles.sub_title}>
-          우리가 다시 대화할 수 있는 작은 기적
-        </h4>
+        <h4 className={styles.sub_title}>우리가 다시 대화할 수 있는 작은 기적</h4>
       </div>
 
       <div className={styles.Social_Login}>
-        <button
-          className={styles.Social_Login_Button}
-          onClick={handleKakaoLogin}
-        >
+        <button className={styles.Social_Login_Button} onClick={handleKakaoLogin}>
           카카오로 로그인
           <img
             src="https://raw.githubusercontent.com/AI-himedia/Final_Project_Assets/main/btn_kakao.svg"
@@ -31,9 +26,7 @@ export default function LoginPage() {
             alt="Kakao Login"
           />
         </button>
-        <p className={styles.Social_Login_Description}>
-          카카오 계정으로 간편하게 시작하세요
-        </p>
+        <p className={styles.Social_Login_Description}>카카오 계정으로 간편하게 시작하세요</p>
       </div>
     </div>
   );
