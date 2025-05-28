9b61c5f62b580f38bf2495e43d49043111bfa5cb
dijeungi <dijeungi@naver.com>
2025-05-28 23:58:44 +0000

    🔧 Project idx 툴 사용으로 인한 커스텀 주소 추가 | 예외처리

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
+// @ts-ignore
+
 // next.config.ts
 // Next.js 프로젝트 전역 설정 파일
 // 기본적으로 건드리지 않아도 되며, 필요 시 옵션 추가
 
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // 예: 이미지 도메인 설정, 리다이렉트, 환경변수 공개 등 추가 가능
+const nextConfig = {
+  allowedDevOrigins: [
+    'https://3000-firebase-againhellogit-1747715584709.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
