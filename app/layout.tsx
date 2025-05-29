4b1acaf3d2068b25ee67f48c3d36b64652c48c86
dijeungi <dijeungi@naver.com>
2025-05-29 04:30:41 +0000

    ⚡️ ReactQueryProvider 파일 추가 및 전역 관리 코드 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
-import './reset.css';
+import './globals.css';
 import '@/_config/fonts';
 import { omyuPretty } from '@/_config/fonts';
 import { ReactNode } from 'react';
+import HeaderController from './_components/header/HeaderController';
+import ReactQueryProvider from './_providers/ReactQueryProvider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko" className={`${omyuPretty.variable}`}>
-      <body>{children}</body>
+      <body>
+        <ReactQueryProvider>
+          <HeaderController />
+          {children}
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
