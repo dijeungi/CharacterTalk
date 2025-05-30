c34d7617ea4b28b39498cc540d2d54a1da0cd615
dijeungi <dijeungi@naver.com>
2025-05-30 01:06:30 +0000

    🎨 폰트 설정 추가 및 전체 사아즈 고정을 위한 style 추

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import './globals.css';
+import './reset.css';
 import '@/_config/fonts';
-import { omyuPretty } from '@/_config/fonts';
+import { omyuPretty, pretendard } from '@/_config/fonts';
 import { ReactNode } from 'react';
 import HeaderController from './_components/header/HeaderController';
 import ReactQueryProvider from './_providers/ReactQueryProvider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko" className={`${omyuPretty.variable}`}>
-      <body>
+    <html lang="ko" className={`${omyuPretty.variable} ${pretendard.variable}`}>
+      <body className="container">
         <ReactQueryProvider>
           <HeaderController />
           {children}
