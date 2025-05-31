4a204f5fbc5086dc98d3ef1aee5bac0870c9cbde
dijeungi <dijeungi@naver.com>
2025-05-31 07:55:35 +0000

    ⚡️ 각 컴포넌트 page에 대한 header config 파일 추가 및 수정

---
+++ app/_config/headerConfig.ts
@@ -0,0 +1,20 @@
+export const headerConfig = [
+  {
+    path: '/login',
+    visible: false,
+  },
+  {
+    path: '/signup',
+    visible: false,
+  },
+  {
+    path: '/admin',
+    visible: true,
+    variant: 'admin',
+  },
+  {
+    path: '/',
+    visible: true,
+    variant: 'default',
+  },
+];
