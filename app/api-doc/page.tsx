85f8e0e3bf5dde2167f1c16d99729d180ede2e50
dijeungi <dijeungi@naver.com>
2025-05-31 07:53:55 +0000

    ✨ api doc를 위한 Swagger 라이브러리 (의존성) 설정 추가

---
+++ app/api-doc/page.tsx
@@ -0,0 +1,12 @@
+import { getApiDocs } from '@/lib/swagger';
+import ReactSwagger from '@/_components/ReactSwagger';
+
+export default async function ApiDocPage() {
+  const spec = await getApiDocs();
+
+  return (
+    <section style={{ padding: '2rem' }}>
+      <ReactSwagger spec={spec} />
+    </section>
+  );
+}
