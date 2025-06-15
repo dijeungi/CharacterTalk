5b700b37e1b8d51d1b061f5277e9bdeeb0e2f4a0
dijeungi <dijeungi@naver.com>
2025-06-15 16:29:19 +0900

    💡 주석 제거

--- frontend/app/(routes)/(public)/login/layout.tsx
+++ frontend/app/(routes)/(public)/login/layout.tsx
@@ -1,8 +1,7 @@
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      {/* <header>로그인 전용 헤더</header> */}
+    <>
       <main>{children}</main>
-    </div>
+    </>
   );
 }
