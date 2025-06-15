f404e565f1d3c67ffbee2097d996a61dbc66f453
dijeungi <dijeungi@naver.com>
2025-06-15 16:21:40 +0900

    🔥 사용 안하는 코드 임시 주석

--- frontend/app/_components/characters/Step2_.tsx
+++ frontend/app/_components/characters/Step2_.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import styles from '/app/(routes)/(private)/characters/new/CharactersNew.module.css';
+// import styles from '/app/(routes)/(private)/characters/new/page.module.css';
 
-export default function Step1_Profile() {
-  return <></>;
-}
+// export default function Step1_Profile() {
+//   return <></>;
+// }
