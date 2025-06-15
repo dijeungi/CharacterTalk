419c38b1e44825d43bbf7f2b0d6b632fa3b907af
dijeungi <dijeungi@naver.com>
2025-06-15 15:15:31 +0900

    ♻️  코드 리팩토링

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,29 +4,28 @@
   Description:
     - 이 페이지는 애플리케이션의 루트 레이아웃을 정의합니다.
     - 전역 스타일(초기화 및 글로벌 스타일)과 리액트 쿼리 프로바이더를 포함하여 애플리케이션의 데이터 관리와 UI 레이아웃을 설정합니다.
-    - `useAuthRestore` 훅을 호출하여 사용자 인증 상태를 복원하고, 인증된 상태에 따라 페이지 렌더링을 조정합니다.
     - 헤더를 포함한 전체 페이지의 레이아웃을 구성하며, 자식 컴포넌트를 동적으로 렌더링합니다.
 */
 
 'use client';
 
-// Css
 import './reset.css';
 import './globals.css';
 
-// React
+// default
 import { ReactNode } from 'react';
 
-// Components
+// components
 import HeaderController from './_components/header/HeaderController';
 
-// Provider
+// provider
 import ReactQueryProvider from './_providers/ReactQueryProvider';
 
-// Custom Hooks - Auth
+// custom hooks
 import { useAuthRestore } from './_hooks/useAuthRestore';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
+  // useAuthRestore = 사용자 인증 상태를 복원하고, 인증된 상태에 따라 페이지 렌더링을 조정
   useAuthRestore();
   return (
     <html lang="ko">
