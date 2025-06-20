251338fb56c4b959645b830443ac927295263dfd
dijeungi <dijeungi@naver.com>
2025-06-20 12:36:52 +0900

    🎨 MUI 로 복원 및 거의 완성판

--- frontend/app/_components/characters/Step1_Profile.tsx
+++ frontend/app/_components/characters/Step1_Profile.tsx
@@ -2,7 +2,7 @@
   Route: '/characters/new'
   Path: app/_components/characters/Step1_Profile.tsx
   Description:
-    -
+    - 캐릭터 생성 step1 페이지
 */
 
 'use client';
@@ -18,21 +18,9 @@ import { MdOutlineFileUpload } from 'react-icons/md';
 import { PiMagicWandDuotone } from 'react-icons/pi';
 import { FaChevronRight } from 'react-icons/fa';
 
-// shadcn/ui
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
-
 // components
 import CharacterPolicyNotice from './Drawer/CharacterPolicyNotice';
+import ContinueCreationModal from './Modal/ContinueCreationModal';
 import VoiceSelectModal from './Modal/VoiceSelectModal';
 
 // store
@@ -42,12 +30,15 @@ import ProfileImageGeneratorDrawer from './Drawer/ProfileImageGeneratorDrawer';
 export default function Step1_Profile() {
   // 상태 초기화
   const [imageGeneratorDrawerOpen, setImageGeneratorDrawerOpen] = useState(false);
+  const [continueModalOpen, setContinueModalOpen] = useState(false);
   const [voiceModalOpen, setVoiceModalOpen] = useState(false);
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
   const [isDataLoaded, setIsDataLoaded] = useState(false);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  // Zustand 상태관리
+  /*
+    - Zustand 상태관리
+    - CharacterCreate.d.ts > characterStep1Store.ts > Step1_Profile.tsx 타입 선언 전달
+  */
   const {
     name,
     oneliner,
@@ -105,7 +96,7 @@ export default function Step1_Profile() {
     }
   };
 
-  // "새로 제작" 핸들러
+  // 새로 제작
   const handleNewCreation = () => {
     localStorage.removeItem('tempCharacterData');
     setName('');
@@ -113,12 +104,12 @@ export default function Step1_Profile() {
     setSelectedVoice('');
     setProfileImage(null);
     resetDirty();
-    setIsDataLoaded(true);
+    setContinueModalOpen(false);
   };
 
-  // "이어서 제작" 핸들러
+  // 이어서 제작
   const handleContinueCreation = () => {
-    setIsDataLoaded(true);
+    setContinueModalOpen(false);
   };
 
   useEffect(() => {
@@ -131,165 +122,153 @@ export default function Step1_Profile() {
       if (parsedData.profileImage) {
         setProfileImage(parsedData.profileImage);
       }
-      setIsAlertOpen(true);
+      setIsDataLoaded(true);
+      setContinueModalOpen(true);
     } else {
       setIsDataLoaded(true);
     }
   }, []);
 
   return (
-    <section className={styles.container}>
-      <div className={styles.formContent}>
-        {/* isDataLoaded가 true일 때만 전체 컨텐츠를 렌더링 */}
-        {isDataLoaded && (
-          <>
-            {/* 프로필 사진 업로드 */}
-            <div className={styles.field}>
-              <label className={styles.label}>
-                프로필 사진 <span className={styles.required}>*</span>
-              </label>
+    <>
+      <section className={styles.container}>
+        <div className={styles.formContent}>
+          {/* isDataLoaded가 true일 때만 전체 컨텐츠를 렌더링 */}
+          {isDataLoaded && (
+            <>
+              {/* 프로필 사진 업로드 */}
+              <div className={styles.field}>
+                <label className={styles.label}>
+                  프로필 사진 <span className={styles.required}>*</span>
+                </label>
 
-              <div className={styles.profileRow}>
-                <div className={styles.imageBox}>
-                  {imagePreview && (
-                    <img src={imagePreview} alt="Profile" className={styles.profileImage} />
-                  )}
-                </div>
-
-                <div className={styles.profileRight}>
-                  <div className={styles.captionBox}>
-                    <p className={styles.caption}>이미지를 필수로 등록해 주세요.</p>
-                    <p className={styles.subCaption}>부적절한 이미지는 업로드가 제한됩니다.</p>
+                <div className={styles.profileRow}>
+                  <div className={styles.imageBox}>
+                    {imagePreview && (
+                      <img src={imagePreview} alt="Profile" className={styles.profileImage} />
+                    )}
                   </div>
-                  <div className={styles.buttonRow}>
-                    <Button variant="outline" type="button" onClick={triggerFileInput}>
-                      <MdOutlineFileUpload className="mr-2 h-4 w-4" /> 업로드
-                      <input
-                        ref={fileInputRef}
-                        id="profile-image-upload"
-                        type="file"
-                        accept="image/*"
-                        style={{ display: 'none' }}
-                        onChange={handleProfileImageUpload}
-                      />
-                    </Button>
-                    <Button
-                      variant="outline"
-                      type="button"
-                      onClick={() => setImageGeneratorDrawerOpen(true)}
-                    >
-                      <PiMagicWandDuotone className="mr-2 h-4 w-4" /> 생성
-                    </Button>
+
+                  <div className={styles.profileRight}>
+                    <div className={styles.captionBox}>
+                      <p className={styles.caption}>이미지를 필수로 등록해 주세요.</p>
+                      <p className={styles.subCaption}>부적절한 이미지는 업로드가 제한됩니다.</p>
+                    </div>
+                    <div className={styles.buttonRow}>
+                      <button className={styles.button} type="button" onClick={triggerFileInput}>
+                        <MdOutlineFileUpload /> 업로드
+                        <input
+                          ref={fileInputRef}
+                          id="profile-image-upload"
+                          type="file"
+                          accept="image/*"
+                          style={{ display: 'none' }}
+                          onChange={handleProfileImageUpload}
+                        />
+                      </button>
+                      <button
+                        className={styles.button}
+                        type="button"
+                        onClick={() => setImageGeneratorDrawerOpen(true)}
+                      >
+                        <PiMagicWandDuotone /> 생성
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            {/* 이름 입력 */}
-            <div className={styles.field}>
-              <label className={styles.label}>
-                이름 <span className={styles.required}>*</span>
-              </label>
-              <p className={styles.caption}>2~20자 이내로 입력해 주세요 (특수문자, 이모지 제외)</p>
-              <div className={styles.inputWrapper}>
-                <input
-                  type="text"
-                  value={name}
-                  maxLength={20}
-                  className={styles.input}
-                  onChange={e => {
-                    if (e.target.value.length <= 20) setName(e.target.value);
-                  }}
-                  placeholder="짧은 이름이 부르기 더 좋아요!"
-                />
-                <div className={styles.charCount}>{name.length} / 20</div>
+              {/* 이름 입력 */}
+              <div className={styles.field}>
+                <label className={styles.label}>
+                  이름 <span className={styles.required}>*</span>
+                </label>
+                <p className={styles.caption}>
+                  2~20자 이내로 입력해 주세요 (특수문자, 이모지 제외)
+                </p>
+                <div className={styles.inputWrapper}>
+                  <input
+                    type="text"
+                    value={name}
+                    maxLength={20}
+                    className={styles.input}
+                    onChange={e => {
+                      if (e.target.value.length <= 20) setName(e.target.value);
+                    }}
+                    placeholder="짧은 이름이 부르기 더 좋아요!"
+                  />
+                  <div className={styles.charCount}>{name.length} / 20</div>
+                </div>
               </div>
-            </div>
 
-            {/* 한 줄 소개 입력 */}
-            <div className={styles.field}>
-              <label className={styles.label}>
-                한 줄 소개 <span className={styles.required}>*</span>
-              </label>
-              <div className={styles.textareaWrapper}>
-                <textarea
-                  value={oneliner}
-                  maxLength={300}
-                  className={styles.textarea}
-                  onChange={e => {
-                    if (e.target.value.length <= 300) setOneliner(e.target.value);
-                  }}
-                  placeholder="어떤 캐릭터인지 간단한 소개 설명을 입력해 주세요."
-                  rows={3}
-                />
-                <div className={styles.textareaCharCount}>{oneliner.length} / 300</div>
+              {/* 한 줄 소개 입력 */}
+              <div className={styles.field}>
+                <label className={styles.label}>
+                  한 줄 소개 <span className={styles.required}>*</span>
+                </label>
+                <div className={styles.textareaWrapper}>
+                  <textarea
+                    value={oneliner}
+                    maxLength={300}
+                    className={styles.textarea}
+                    onChange={e => {
+                      if (e.target.value.length <= 300) setOneliner(e.target.value);
+                    }}
+                    placeholder="어떤 캐릭터인지 간단한 소개 설명을 입력해 주세요."
+                    rows={3}
+                  />
+                  <div className={styles.textareaCharCount}>{oneliner.length} / 300</div>
+                </div>
               </div>
-            </div>
 
-            {/* 목소리 선택 */}
-            <div className={styles.field}>
-              <label className={styles.label}>목소리 선택</label>
-              <p className={styles.caption}>캐릭터에게 어울리는 목소리를 선택해 주세요</p>
-              <button
-                className={styles.voiceSelectedButton}
-                onClick={() => setVoiceModalOpen(true)}
-                type="button"
-              >
-                {selectedVoice
-                  ? selectedVoice.replace('_', ' ')
-                  : '캐릭터의 목소리를 선택해주세요.'}
-                <FaChevronRight className={styles.chevronIcon} />
-              </button>
-            </div>
+              {/* 목소리 선택 */}
+              <div className={styles.field}>
+                <label className={styles.label}>목소리 선택</label>
+                <p className={styles.caption}>캐릭터에게 어울리는 목소리를 선택해 주세요</p>
+                <button
+                  className={styles.voiceSelectedButton}
+                  onClick={() => setVoiceModalOpen(true)}
+                  type="button"
+                >
+                  {selectedVoice
+                    ? selectedVoice.replace('_', ' ')
+                    : '캐릭터의 목소리를 선택해주세요.'}
+                  <FaChevronRight className={styles.chevronIcon} />
+                </button>
+              </div>
 
-            <CharacterPolicyNotice />
+              <CharacterPolicyNotice />
 
-            <div className={styles.fixedBottom}>
-              <Button className={styles.Button} disabled={!isFormValid} size="lg">
-                다음 단계
-              </Button>
-            </div>
+              <div className={styles.fixedBottom}>
+                <button className={styles.Button} disabled={!isFormValid}>
+                  다음 단계
+                </button>
+              </div>
 
-            <VoiceSelectModal
-              open={voiceModalOpen}
-              onClose={() => setVoiceModalOpen(false)}
-              selectedVoice={selectedVoice}
-              setSelectedVoice={setSelectedVoice}
-            />
+              <VoiceSelectModal
+                open={voiceModalOpen}
+                onClose={() => setVoiceModalOpen(false)}
+                selectedVoice={selectedVoice}
+                setSelectedVoice={setSelectedVoice}
+              />
 
-            <ProfileImageGeneratorDrawer
-              open={imageGeneratorDrawerOpen}
-              onClose={() => setImageGeneratorDrawerOpen(false)}
-              onImageGenerated={imageFile => {
-                setProfileImage(imageFile);
-                setImageGeneratorDrawerOpen(false);
-              }}
-            />
-          </>
-        )}
-      </div>
-
-      {/* 이어서/새로 제작 선택 AlertDialog */}
-      <AlertDialog open={isAlertOpen} onOpenChange={setIsAlertOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>작성 이어하기</AlertDialogTitle>
-            <AlertDialogDescription>
-              임시 저장된 데이터가 있습니다. 이어서 작성하시겠습니까?
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel asChild>
-              <Button variant="outline" onClick={handleNewCreation}>
-                새로 제작
-              </Button>
-            </AlertDialogCancel>
-            <AlertDialogAction asChild>
-              <Button onClick={handleContinueCreation}>이어서 제작</Button>
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </section>
+              <ProfileImageGeneratorDrawer
+                open={imageGeneratorDrawerOpen}
+                onClose={() => setImageGeneratorDrawerOpen(false)}
+                onImageGenerated={imageFile => {
+                  setProfileImage(imageFile);
+                  setImageGeneratorDrawerOpen(false);
+                }}
+              />
+            </>
+          )}
+        </div>
+      </section>
+      <ContinueCreationModal
+        open={continueModalOpen}
+        onNew={handleNewCreation}
+        onContinue={handleContinueCreation}
+      />
+    </>
   );
 }
