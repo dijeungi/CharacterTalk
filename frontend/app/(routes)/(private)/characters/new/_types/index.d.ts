8c16cf06b848027a20ed0af598ad1f4ef2a2686f
dijeungi <dijeungi@naver.com>
2025-06-27 04:34:00 +0900

    🏷️ 타입 추가 및 주석 경로 추가

--- frontend/app/(routes)/(private)/characters/new/_types/index.d.ts
+++ frontend/app/(routes)/(private)/characters/new/_types/index.d.ts
@@ -1,3 +1,15 @@
+interface Step1Props {
+  onNext: () => void;
+}
+
+// frontend/app/(routes)/(private)/characters/new/_components/Modal/ContinueCreationModal.tsx
+interface ContinueCreationModalProps {
+  open: boolean;
+  onNew: () => void;
+  onContinue: () => void;
+}
+
+// frontend/app/(routes)/(private)/characters/new/_components/Drawer/ProfileImageGeneratorDrawer.tsx
 interface ProfileImageGeneratorDrawerProps {
   open: boolean;
   onClose: () => void;
