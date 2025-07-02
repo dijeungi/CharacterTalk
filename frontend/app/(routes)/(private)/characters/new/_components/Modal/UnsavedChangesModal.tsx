a73bb3c2400c6da834ea0641461559a95bd44ef9
dijeungi <dijeungi@naver.com>
2025-07-02 10:49:04 +0900

    🎨 css 파일 및 div className 통합

--- frontend/app/(routes)/(private)/characters/new/_components/Modal/UnsavedChangesModal.tsx
+++ frontend/app/(routes)/(private)/characters/new/_components/Modal/UnsavedChangesModal.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import styles from './UnsavedChangesModal.module.css';
+import styles from './page.module.css';
 
 const UnsavedChangesModal: React.FC<UnsavedChangesModalProps> = ({ onClose, onExit }) => {
   return (
-    <div className={styles.overlay}>
-      <div className={styles.modal}>
-        <h2 className={styles.title}>캐릭터 설정 내용이 저장되지 않았어요</h2>
+    <div className={styles.container}>
+      <div
+        className={styles.content}
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="dialogTitle"
+        aria-description="dialogDesc"
+        tabIndex={-1}
+      >
+        <h2 className={styles.title}>캐릭터 설정 내용이 저장되지 않았어요 ❌</h2>
         <p className={styles.description}>
           임시저장하지 않으면
           <br />
