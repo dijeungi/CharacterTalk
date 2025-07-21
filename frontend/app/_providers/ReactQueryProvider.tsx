a4f19f0b3c95820997a95638a9474eeee4771e05
dijeungi <dijeungi@naver.com>
2025-07-21 15:42:59 +0900

    💡 Provider : 주석 문서화

--- frontend/app/_providers/ReactQueryProvider.tsx
+++ frontend/app/_providers/ReactQueryProvider.tsx
@@ -1,20 +1,19 @@
-/*
-  : React Queyry Provider
-  app/_providers/ReactQueryProvider.tsx
-*/
+/**
+ * @file      frontend/app/_providers/ReactQueryProvider.tsx
+ * @desc      Provider: React Query의 전역 상태 관리를 위한 QueryClient 설정 및 주입
+ *
+ * @author    최준호
+ * @update    2025.07.21
+ */
 
 'use client';
-
-// React
 import { useState } from 'react';
 
-// React Query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   // QueryClient 생성
   const [client] = useState(() => new QueryClient());
-
   // QueryClientProvider로 자식(모든 컴포넌트)에 쿼리 상태 제공
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
