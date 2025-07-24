903c801bc12e3f2848c0a31d778b77b8a458338b
dijeungi <dijeungi@naver.com>
2025-07-24 15:52:14 +0900

    🚚 절대경로 수정 및 코드 문서화 주석 추가

--- frontend/app/(routes)/(private)/characters/new/_hooks/useCharacterStepCommon.ts
+++ frontend/app/(routes)/(private)/characters/new/_hooks/useCharacterStepCommon.ts
@@ -1,6 +1,16 @@
-// /_hooks/characters/useCharacterStepCommon.ts
-import { useCharacterCreationStore } from '@/app/_store/characters';
+/**
+ * @file         frontend/app/(routes)/(private)/characters/new/_hooks/useCharacterStepCommon.ts
+ * @desc         캐릭터 생성 단계의 값 변경 여부를 감지해 isDirty(input[Form]) 상태를 자동으로 설정하는 훅
+ *
+ * @author       최준호
+ * @update       2025.07.22
+ */
+
+'use client';
 import { useEffect } from 'react';
+
+import { useCharacterCreationStore } from '@/app/_store/characters';
+import { CharacterCreationState } from '@/app/_store/characters/types';
 
 // Step들의 Form 필드의 수정 여부를 감지해 isDirty 상태를 자동으로 설정/해제하는 공통 훅
 export function useCharacterStepCommon(affectedKeys: (keyof CharacterCreationState)[]) {
