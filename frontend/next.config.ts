2213748bf543a87be90efc96a02dbc0878048fc5
dijeungi <dijeungi@naver.com>
2025-07-30 09:45:45 +0900

    🔧 reactStrictMode false로 새로고침 2번 제거 (무쓸모)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,7 @@
 // 기본적으로 건드리지 않아도 되며, 필요 시 옵션 추가
 
 const nextConfig = {
+  reactStrictMode: false,
   allowedDevOrigins: [
     'https://3000-firebase-charactertalkgit-1747715584709.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev',
   ],
